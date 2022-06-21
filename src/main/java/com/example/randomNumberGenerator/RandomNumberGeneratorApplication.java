8f3dd7cde43f526e6baf073e706d86dd98401695
AndreasFeiertag <andreasfeiertag@outlook.at>
2022-06-21 22:10:59 +0200

    Comments added

--- src/main/java/com/example/randomNumberGenerator/RandomNumberGeneratorApplication.java
+++ src/main/java/com/example/randomNumberGenerator/RandomNumberGeneratorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RandomNumberGeneratorApplication {
-
+//main application
 	public static void main(String[] args) {
 		SpringApplication.run(RandomNumberGeneratorApplication.class, args);
 	}
